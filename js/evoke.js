44d0aa4a8ea98160037eb4e6f51d75f4036b7f19
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2016-02-16 18:44:51 +0900

    debug

--- js/evoke.js
+++ js/evoke.js
@@ -1,19 +1,6 @@
 $(function(){
-    var opts_en = {
-        language: "en",
-        pathPrefix: "json"
-    };
-    var opts_ja = {
-        language: "ja",
-        pathPrefix: "json"
-    };
-    // How can I get the language that is currently used?
-    $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "ja"});
     $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "en"});
     $("#change-lang").click(function(){
-        console.log("A");
-        console.log($("#cur-lang").text());
-        console.log($("#cur-lang").text() == "ja");
         if ($("#cur-lang").text() == "ja") {
             $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "en"});
         } else {
