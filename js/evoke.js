eea3372672258b2ca63ba9bc5139ba9234b08341
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2016-02-16 18:36:45 +0900

    revise

--- js/evoke.js
+++ js/evoke.js
@@ -10,6 +10,9 @@ $(function(){
     // How can I get the language that is currently used?
     $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "en"});
     $("#change-lang").click(function(){
+        console.log("A");
+        console.log($("#cur-lang").text());
+        console.log($("#cur-lang").text() == "ja");
         if ($("#cur-lang").text() == "ja") {
             $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "en"});
         } else {
