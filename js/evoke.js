815dc6b017d221cf9f198ae72ae20eaf8b4304bf
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2016-02-16 18:28:36 +0900

    refactor;

--- js/evoke.js
+++ js/evoke.js
@@ -1,7 +1,19 @@
 $(function(){
-    var opts = {
-        skipLanguage : "en",
+    var opts_en = {
+        language: "en",
         pathPrefix: "json"
     };
-    $("[data-localize]").localize("mypage", opts);
+    var opts_ja = {
+        language: "ja",
+        pathPrefix: "json"
+    };
+    // How can I get the language that is currently used?
+    $("[data-localize]").localize("mypage", {pathPrefix: "json"});
+    $("#change-lang").click(function(){
+        if ($("#cur-lang").text() == "ja") {
+            $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "en"});
+        } else {
+            $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "ja"});
+        }
+    });
 });
