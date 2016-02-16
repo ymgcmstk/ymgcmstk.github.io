daca6cfe96698166dd427239e3a2ed2429fd1af7
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2016-02-16 19:05:01 +0900

    refactor

--- js/evoke.js
+++ js/evoke.js
@@ -2,9 +2,9 @@ $(function(){
     $("[data-localize]").localize("mypage", {pathPrefix: "json"});
     $("#change-lang").click(function(){
         if ($("#cur-lang").text() == "ja") {
-            $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "en", skipLanguage: "ja"});
+            $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "en"});
         } else {
-            $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "ja", skipLanguage: "en"});
+            $("[data-localize]").localize("mypage", {pathPrefix: "json", language: "ja"});
         }
     });
 });
